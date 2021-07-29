6f2955aa95b8bc63509e55dddc055255c3d68a53
armin jahani <arminjhni@gmail.com>
2021-07-29 06:44:52 +0430

    add li-hover style because after user clicks on the options remove li-hover

--- app.js
+++ app.js
@@ -8,32 +8,41 @@ let ul = document.querySelector('ul');
 const quescounter = document.querySelector('.question-counter');
 var timer = document.querySelector('#timer');
 
-
+// number of the questions 
 function quesCounter (index) {
     quescounter.innerHTML = `<strong>${questions[index].numb}</strong> of <strong>${questions.length}</strong> Questions`;
 }
-function quesHandler (index) {
-    questionTitle.innerHTML = `<span>${questions[index-1].numb}.</span> ${questions[index-1].question}`;
-    for (let i=0; i< questions[index-1].options.length; i++){
+// show the questions in li tag and set attributer for eachone 
+function quesHandler (numbIndex) {
+    let quesindex  = numbIndex - 1
+    questionTitle.innerHTML = `<span>${questions[quesindex].numb}.</span> ${questions[quesindex].question}`;
+    for (let i=0; i< questions[quesindex].options.length; i++){
         var li = document.createElement('li');
-        li.innerHTML = (questions[index-1].answer === questions[index-1].options[i]) ? 
-        `${questions[index-1].options[i]} <span class='check-icon'><i class="fas fa-check"></i></span>`: 
-        `${questions[index-1].options[i]} <span class='times-icon'><i class="fas fa-times"></i></span>` ;
+        li.classList.add('li-hover')
+        li.innerHTML = (questions[quesindex].answer === questions[quesindex].options[i]) ? 
+        `${questions[quesindex].options[i]} <span class='check-icon'><i class="fas fa-check"></i></span>`: 
+        `${questions[quesindex].options[i]} <span class='times-icon'><i class="fas fa-times"></i></span>` ;
         ul.appendChild(li);
-        li.setAttribute('onclick', 'ansSelect(this, 0)')
+        li.setAttribute('onclick', `ansSelect(this, ${quesindex})`)
     }
 }
+// handle the user selection 
 function ansSelect(userAnswer, ansIndex) {
+    // disable all options after ckicked on the option 
     userAnswer.style.border = 'none';
     for (let i=0; i<4; i++){
         ul.childNodes[i].style.cursor = 'default'
+        ul.childNodes[i].classList.remove('li-hover')
         ul.childNodes[i].setAttribute('onclick', 'function userClick(){return null}')
     }
+    // when user click on the right answer 
    if (userAnswer.innerText === questions[ansIndex].answer){
         const checkIcon = document.querySelector('.check-icon');
         checkIcon.style.display = 'block';
         userAnswer.style.backgroundColor = 'rgb(203, 241, 200)';
-    } else{
+    } 
+    // when click on the uncorrect answer 
+    else{
         const timesIcon = document.querySelectorAll('.times-icon')
         for (let i=0; i<3; i++){
             if (timesIcon[i].previousSibling.textContent === userAnswer.textContent){
@@ -43,15 +52,17 @@ function ansSelect(userAnswer, ansIndex) {
         userAnswer.style.backgroundColor = 'rgb(255, 198, 198)'
     }
 }
-
+// user click on the Start Quiz 
 start.onclick = function() {
     start.style.display ='none';
     info.style.display = 'flex';
 };
+// user click on the Exit Quiz 
 exitQuiz.onclick = function() {
     start.style.display = 'block';
     info.style.display = 'none';
 };
+// user click on the continue 
 cntinue.onclick = function() {
     info.style.display = 'none';
     infoContainer.style.display = 'block';
